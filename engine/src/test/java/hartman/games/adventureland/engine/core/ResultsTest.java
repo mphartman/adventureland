2673d50d5376d876c092e29568b59c3472dfddbb
Michael Hartman <mphartman@me.com>
2018-08-04 16:02:27 -0500

    Test for Goto Room result.

---
+++ engine/src/test/java/hartman/games/adventureland/engine/core/ResultsTest.java
@@ -0,0 +1,31 @@
+package hartman.games.adventureland.engine.core;
+
+import org.junit.Test;
+
+import hartman.games.adventureland.engine.GameState;
+import hartman.games.adventureland.engine.Player;
+import hartman.games.adventureland.engine.PlayerCommand;
+import hartman.games.adventureland.engine.Room;
+import org.junit.Assert;
+
+public class ResultsTest {
+    
+    @Test
+    public void gotoRoomShouldMovePlayerInDirectionOfGivenNoun() {
+        Room tower_second_floor = new Room("tower_second_floor", "Second story room of the tower.");
+        Room tower_first_floor = new Room("tower_first_floor", "The first floor of a tall stone tower.", new Room.Exit.Builder().exit(Nouns.UP).towards(tower_second_floor).build());
+        GameState gameState = new GameState(new Player("Archie"), tower_first_floor);
+        PlayerCommand playerCommand = new PlayerCommand(Verbs.GO, Nouns.UP);
+        Results.GOTO_ROOM.accept(playerCommand, gameState);
+
+        Assert.assertEquals(tower_second_floor, gameState.getPlayerCurrentPosition());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void gotoRoomShouldThrowExceptionIfDirectionIsNotValidExitFromCurrentRoom() {
+        Room sealed_tomb = new Room("sealed_tomb", "There is no escape.");
+        GameState gameState = new GameState(new Player("Archie"), sealed_tomb);
+        PlayerCommand playerCommand = new PlayerCommand(Verbs.GO, Nouns.UP);
+        Results.GOTO_ROOM.accept(playerCommand, gameState);
+    }
+}
