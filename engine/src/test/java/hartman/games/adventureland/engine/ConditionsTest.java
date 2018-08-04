cf0cb9ef6b6952c529d15b486dc8ea43ce0caa6e
Michael Hartman <mphartman@me.com>
2018-08-03 20:44:17 -0500

    Tests for IN ROOM condition

--- engine/src/test/java/hartman/games/adventureland/engine/ConditionsTest.java
+++ engine/src/test/java/hartman/games/adventureland/engine/ConditionsTest.java
@@ -1,5 +1,6 @@
 package hartman.games.adventureland.engine;
 
+import hartman.games.adventureland.engine.Action.Condition;
 import hartman.games.adventureland.engine.core.Conditions;
 import hartman.games.adventureland.engine.core.Nouns;
 import hartman.games.adventureland.engine.core.Verbs;
@@ -33,5 +34,23 @@ public class ConditionsTest {
         assertFalse(Conditions.HAS_EXIT.apply(playerCommand));
     }
 
+    @Test
+    public void inRoomShouldReturnFalseWhenPlayerIsNotInRoom() {
+        Room start = new Room("start", "start");
+        Player player = new Player("Archie");
+        GameState gameState = new GameState(player, start);
+        PlayerCommand playerCommand = new PlayerCommand(Verbs.GO, Nouns.ANY, gameState);
+        Condition inRoom = new Conditions.IN_ROOM(Room.NOWHERE);
+        assertFalse(inRoom.apply(playerCommand));
+    }
 
+    @Test
+    public void inRoomShouldReturnTrueWhenPlayerIsInRoom() {
+        Room start = new Room("start", "start");
+        Player player = new Player("Archie");
+        GameState gameState = new GameState(player, start);
+        PlayerCommand playerCommand = new PlayerCommand(Verbs.GO, Nouns.ANY, gameState);
+        Condition inRoom = new Conditions.IN_ROOM(start);
+        assertTrue(inRoom.apply(playerCommand));
+    }
 }
