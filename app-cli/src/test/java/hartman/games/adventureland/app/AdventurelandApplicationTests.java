4fa48861779bbc475c1b0ac0a438fba15cbc1859
Hartman <mphartman1@gmail.com>
2018-08-07 12:47:01 -0500

    Remove test because it causes timeout waiting for user input.

--- app-cli/src/test/java/hartman/games/adventureland/app/AdventurelandApplicationTests.java
+++ app-cli/src/test/java/hartman/games/adventureland/app/AdventurelandApplicationTests.java
@@ -1,6 +1,5 @@
 package hartman.games.adventureland.app;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,9 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AdventurelandApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
