acccf552827db7d484274e8b4d9a91e56a7d496e
Hartman <mphartman1@gmail.com>
2018-08-07 15:03:51 -0500

    Tweak to make room display work.

--- app-cli/src/main/java/hartman/games/adventureland/app/AdventurelandApplication.java
+++ app-cli/src/main/java/hartman/games/adventureland/app/AdventurelandApplication.java
@@ -1,38 +1,63 @@
 package hartman.games.adventureland.app;
 
-import hartman.games.adventureland.engine.*;
+import hartman.games.adventureland.engine.Action;
+import hartman.games.adventureland.engine.Adventure;
+import hartman.games.adventureland.engine.Display;
+import hartman.games.adventureland.engine.Game;
+import hartman.games.adventureland.engine.GameState;
+import hartman.games.adventureland.engine.Interpreter;
+import hartman.games.adventureland.engine.Noun;
+import hartman.games.adventureland.engine.Room;
+import hartman.games.adventureland.engine.Verb;
+import hartman.games.adventureland.engine.Vocabulary;
 import hartman.games.adventureland.engine.core.Actions;
 import hartman.games.adventureland.engine.core.DefaultInterpreter;
 import hartman.games.adventureland.engine.core.Nouns;
+import hartman.games.adventureland.engine.core.Results;
 import hartman.games.adventureland.engine.core.Verbs;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 @SpringBootApplication
 public class AdventurelandApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdventurelandApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdventurelandApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
-	    Adventure adventure = MyAdventures.House_Escape();
-	    Interpreter interpreter = new ConsoleInterpreter(adventure.getVocabulary());
-	    GameState gameState = new GameState(adventure.getStartRoom());
-        Game game = new Game(adventure, interpreter, gameState, new ConsoleDisplay());
+    private static final String introduction = String.format(
+            "House Escape, a text-based adventure.%n" +
+                    "Copyright © 2018, Michael Hartman%n" +
+                    "Distributed under the Apache License, version 2.%n%n" +
+                    "A voice BOOMS out:%n%n" +
+                    "In this adventure you're to escape from the haunted house.  Good luck.%n"
+    );
+
+
+    @Override
+    public void run(String... args) {
+        Adventure adventure = MyAdventures.House_Escape();
+        Interpreter interpreter = new ConsoleInterpreter(adventure.getVocabulary());
+        GameState gameState = new GameState(adventure.getStartRoom());
+        ConsoleDisplay display = new ConsoleDisplay();
+        display.print(introduction);
+        Game game = new Game(adventure, interpreter, gameState, display);
         game.run();
-	}
+        display.print("Goodbye. Thank you for playing. Have a nice day!");
+    }
 }
 
 class MyAdventures {
 
     static Adventure House_Escape() {
 
-        Set<Verb> verbs = Verbs.asSet(Verbs.QUIT, Verbs.GO);
+        Set<Verb> verbs = Verbs.asSet(Verbs.QUIT, Verbs.GO, Verbs.LOOK);
         Set<Noun> nouns = Nouns.directions();
         Vocabulary vocabulary = new Vocabulary(verbs, nouns);
 
@@ -50,9 +75,10 @@ class MyAdventures {
         kitchen.setExit(Nouns.WEST, living_room);
         living_room.setExit(Nouns.EAST, kitchen);
 
-        Set<Action> occurs = Collections.emptySet();
+        Action promptAction = new Action(new Results.PRINT("What should I do?  "));
+        Set<Action> occurs = new HashSet<>(Arrays.asList(promptAction));
 
-        Set<Action> actions = new HashSet<>(Arrays.asList(Actions.QUIT_ACTION, Actions.GO_ACTION));
+        Set<Action> actions = new HashSet<>(Arrays.asList(Actions.QUIT_ACTION, Actions.GO_ACTION, Actions.LOOK_ACTION));
 
         return new Adventure(vocabulary, occurs, actions, hallway);
     }
