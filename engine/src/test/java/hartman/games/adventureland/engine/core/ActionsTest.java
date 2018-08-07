6f44ba2b2858006b438b5ec5da56efc572030d80
Hartman <mphartman1@gmail.com>
2018-08-07 14:15:50 -0500

    Quit action tests.

--- engine/src/test/java/hartman/games/adventureland/engine/core/ActionsTest.java
+++ engine/src/test/java/hartman/games/adventureland/engine/core/ActionsTest.java
@@ -1,15 +1,14 @@
 package hartman.games.adventureland.engine.core;
 
-import hartman.games.adventureland.engine.ActionContext;
-import hartman.games.adventureland.engine.GameState;
-import hartman.games.adventureland.engine.PlayerCommand;
-import hartman.games.adventureland.engine.Room;
+import hartman.games.adventureland.engine.*;
 import org.junit.Test;
 
 import static hartman.games.adventureland.engine.core.Nouns.DOWN;
 import static hartman.games.adventureland.engine.core.Nouns.UP;
 import static hartman.games.adventureland.engine.core.Verbs.GO;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ActionsTest {
 
@@ -38,5 +37,15 @@ public class ActionsTest {
         Actions.GO_ACTION.run(new ActionContext(gameState, msg -> {}, playerCommand));
 
         assertEquals(dungeon, gameState.getCurrentRoom());
+    }
+
+    @Test
+    public void quitActionShouldQuitGivenQuitVerb() {
+        GameState gameState = new GameState(Room.NOWHERE);
+        assertTrue(gameState.isRunning());
+        Actions.QUIT_ACTION.run(new ActionContext(gameState, msg -> {}, new PlayerCommand(new Verb("stop"), Noun.NONE)));
+        assertTrue(gameState.isRunning());
+        Actions.QUIT_ACTION.run(new ActionContext(gameState, msg -> {}, new PlayerCommand(Verbs.QUIT, Noun.NONE)));
+        assertFalse(gameState.isRunning());
     }
 }
