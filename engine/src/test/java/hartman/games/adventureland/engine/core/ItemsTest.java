3f3c72f95c427e4ab7a45ed446d41f25253c414d
Michael Hartman <mphartman@me.com>
2018-08-10 09:31:33 -0500

    Item set test.

--- engine/src/test/java/hartman/games/adventureland/engine/core/ItemsTest.java
+++ engine/src/test/java/hartman/games/adventureland/engine/core/ItemsTest.java
@@ -1,11 +1,21 @@
 package hartman.games.adventureland.engine.core;
 
+import hartman.games.adventureland.engine.Item;
 import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
 
 public class ItemsTest {
 
     @Test
     public void itemSetContainsItemsBuiltUsingIt() {
-        // TODO
+        Items.ItemSet itemSet = Items.newItemSet();
+        Item mug = itemSet.newItem().named("mug").build();
+        Item mouse = itemSet.newItem().named("mouse").build();
+        Set<Item> items = itemSet.copyOfItems();
+        assertTrue(items.contains(mug));
+        assertTrue(items.contains(mouse));
     }
 }
