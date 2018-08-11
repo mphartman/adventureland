089f4838f12e602ba516d2a7842e141cd531c05a
Michael Hartman <mphartman@me.com>
2018-08-11 14:20:08 -0500

    Replace static inner classes for lambdas for conciseness and readability.

--- engine/src/main/java/hartman/games/adventureland/engine/core/Results.java
+++ engine/src/main/java/hartman/games/adventureland/engine/core/Results.java
@@ -1,10 +1,8 @@
 package hartman.games.adventureland.engine.core;
 
 import hartman.games.adventureland.engine.Action.Result;
-import hartman.games.adventureland.engine.Command;
 import hartman.games.adventureland.engine.Display;
 import hartman.games.adventureland.engine.GameElementVisitor;
-import hartman.games.adventureland.engine.GameState;
 import hartman.games.adventureland.engine.Item;
 import hartman.games.adventureland.engine.Noun;
 import hartman.games.adventureland.engine.Room;
@@ -19,78 +17,29 @@ public final class Results {
         throw new IllegalStateException("utility class");
     }
 
+    /**
+     * Indicate that game state is no longer running.
+     */
     public static final Result quit = (command, gameState, display) -> gameState.quit();
 
+    /**
+     * Change current room based on the given Command's Noun.
+     */
     public static final Result go = (command, gameState, display) -> gameState.exitTowards(command.getNoun());
 
     public static final Result goUsingVerb = (command, gameState, display) -> gameState.exitTowards(new Noun(command.getVerb().getName()));
 
-    public static Result look(Look.LookCallback callback) {
-        return new Look(callback);
-    }
-
-    public static Result print(String message) {
-        return new Print(message);
-    }
-
-    public static Result printf(String message, Object... args) {
-        return print(String.format(message, args));
-    }
-
-    public static Result println(String message) {
-        return printf(message + "%n");
-    }
-
-    public static Result inventory(Inventory.InventoryCallback callback) {
-        return new Inventory(callback);
-    }
-
-    public static Result swap(Item item1, Item item2) {
-        return new Swap(item1, item2);
-    }
-
-    public static Result gotoRoom(Room room) {
-        return new Goto(room);
-    }
-
-    public static Result put(Item item, Room room) {
-        return ((command, gameState, display) -> item.drop(room));
-    }
-
-    public static Result putHere(Item item) {
-        return (command, gameState, display) -> item.drop(gameState.getCurrentRoom());
-    }
-
-    public static final Result get = ((command, gameState, display) -> gameState.putInInventory(command.getNoun()));
-
-    public static final Result drop = ((command, gameState, display) -> gameState.drop(command.getNoun()));
-
-    public static Result putWith(Item item1, Item item2) {
-        return new PutWith(item1, item2);
-    }
-
-    public static Result destroy(Item item) {
-        return new Destroy(item);
+    @FunctionalInterface
+    public interface LookCallback {
+        String describe(Room room, List<Room.Exit> exits, List<Item> items);
     }
 
     /**
      * Provides the room, exits, and items to a callback.
      */
-    public static class Look implements Result {
+    public static Result look(LookCallback callback) {
+        return (command, gameState, display) -> {
 
-        @FunctionalInterface
-        public interface LookCallback {
-            String execute(Room room, List<Room.Exit> exits, List<Item> items);
-        }
-
-        private final LookCallback callback;
-
-        Look(LookCallback callback) {
-            this.callback = callback;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
             List<Room> rooms = new ArrayList<>();
             List<Room.Exit> exits = new ArrayList<>();
             List<Item> items = new ArrayList<>();
@@ -112,52 +61,55 @@ public final class Results {
                 }
             });
 
-            String description = callback.execute(
+            String description = callback.describe(
                     rooms.stream().findFirst().orElse(Room.NOWHERE),
                     Collections.unmodifiableList(exits),
                     Collections.unmodifiableList(items));
 
             display.print(description);
-        }
+        };
     }
 
     /**
      * Prints the specified message to the {@link Display}
      */
-    public static class Print implements Result {
-
-        private final String message;
-
-        Print(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
+    public static Result print(String message) {
+        return (command, gameState, display) -> {
             String output = message.replaceAll("\\Q{noun}\\E", command.getNoun().getName());
             output = output.replaceAll("\\Q{verb}\\E", command.getVerb().getName());
             display.print(output);
-        }
+        };
+    }
+
+    /**
+     * Message is passed to String.format
+     *
+     * @see #print(String)
+     */
+    public static Result printf(String message, Object... args) {
+        return print(String.format(message, args));
+    }
+
+    /**
+     * Message has a newline appended.
+     *
+     * @see #printf(String, Object...)
+     */
+    public static Result println(String message) {
+        return printf(message.concat("%n"));
+    }
+
+    @FunctionalInterface
+    public interface InventoryCallback {
+        String execute(List<Item> items);
     }
 
     /**
      * Provides a callback with a list of items that the player carrying.
      */
-    public static class Inventory implements Result {
+    public static Result inventory(InventoryCallback callback) {
+        return (command, gameState, display) -> {
 
-        @FunctionalInterface
-        public interface InventoryCallback {
-            String execute(List<Item> items);
-        }
-
-        private final InventoryCallback callback;
-
-        Inventory(InventoryCallback callback) {
-            this.callback = callback;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
             List<Item> items = new ArrayList<>();
 
             gameState.inventory(new GameElementVisitor() {
@@ -178,77 +130,59 @@ public final class Results {
             });
 
             display.print(callback.execute(items));
-        }
+        };
     }
 
     /**
      * Exchanges the two specified items, so that each occupies the location previously occupied by the other.
      */
-    public static class Swap implements Result {
-
-        private final Item item1;
-        private final Item item2;
-
-        Swap(Item item1, Item item2) {
-            this.item1 = item1;
-            this.item2 = item2;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
-            item1.drop(item2.drop(item1.drop(Room.NOWHERE)));
-        }
+    public static Result swap(Item item1, Item item2) {
+        return (command, gameState, display) -> item1.drop(item2.drop(item1.drop(Room.NOWHERE)));
     }
 
     /**
      * Moves to the specified room
      */
-    public static class Goto implements Result {
-
-        private final Room room;
-
-        Goto(Room room) {
-            this.room = room;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
-            gameState.moveTo(room);
-        }
+    public static Result gotoRoom(Room room) {
+        return (command, gameState, display) -> gameState.moveTo(room);
     }
+
+    /**
+     * Put ITEM in ROOM.
+     */
+    public static Result put(Item item, Room room) {
+        return (command, gameState, display) -> item.drop(room);
+    }
+
+    /**
+     * Put ITEM in current room.
+     */
+    public static Result putHere(Item item) {
+        return (command, gameState, display) -> item.drop(gameState.getCurrentRoom());
+    }
+
+    /**
+     * Put item mentioned in Command Noun in Inventory.
+     */
+    public static final Result get = ((command, gameState, display) -> gameState.putInInventory(command.getNoun()));
+
+    /**
+     * Drop item mentioned in Command Noun in current room.
+     */
+    public static final Result drop = ((command, gameState, display) -> gameState.drop(command.getNoun()));
 
     /**
      * Puts the first-specified item into the same location as the second.
      */
-    public static class PutWith implements Result {
-
-        private final Item item1;
-        private final Item item2;
-
-        public PutWith(Item item1, Item item2) {
-            this.item1 = item1;
-            this.item2 = item2;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
-            this.item1.putWith(item2);
-        }
+    public static Result putWith(Item item1, Item item2) {
+        return (command, gameState, display) -> item1.putWith(item2);
     }
 
     /**
      * Removes ITEM from the game.
      */
-    public static class Destroy implements Result {
-        private final Item item;
-
-        public Destroy(Item item) {
-            this.item = item;
-        }
-
-        @Override
-        public void execute(Command command, GameState gameState, Display display) {
-            gameState.destroy(item);
-        }
+    public static Result destroy(Item item) {
+        return (command, gameState, display) -> gameState.destroy(item);
     }
+
 }
