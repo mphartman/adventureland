2c62e1f5c5b8aa97366db9e4832400125d22d8c7
Michael Hartman <mphartman@me.com>
2018-08-27 20:34:14 -0500

    Provide integration test properties to use embedded H2 database.

--- api/src/test/java/hartman/games/adventureland/api/AbstractIntegrationTest.java
+++ api/src/test/java/hartman/games/adventureland/api/AbstractIntegrationTest.java
@@ -2,10 +2,12 @@ package hartman.games.adventureland.api;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 public abstract class AbstractIntegrationTest {
