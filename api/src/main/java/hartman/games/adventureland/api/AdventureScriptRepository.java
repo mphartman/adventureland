e3b628ed3af9c7e2db116e34f60e26bcc8e37cad
Michael Hartman <mphartman@me.com>
2018-09-05 19:45:04 -0500

    Add missing annotation.

--- api/src/main/java/hartman/games/adventureland/api/AdventureScriptRepository.java
+++ api/src/main/java/hartman/games/adventureland/api/AdventureScriptRepository.java
@@ -2,9 +2,11 @@ package hartman.games.adventureland.api;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 interface AdventureScriptRepository extends CrudRepository<AdventureScript, Long> {
 
     Optional<AdventureScript> findByAdventureId(@Param("adventureId") long adventureId);
